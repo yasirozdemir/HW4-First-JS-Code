b57c2817703b52d6628d964a7723f79f22626ba0
Muhamed Yasir Özdemir <yasirozdmr@yandex.com>
2022-11-28 16:21:58 +0300

    first JS code, and testing it using chrome console

---
+++ name-age.js
@@ -0,0 +1,12 @@
+let name = "Muhammed";
+let midName = "Yasir";
+let lastName = "Ozdemir";
+
+let fullName = name + " " + midName + " " + lastName;
+
+let myAge = 20;
+let currentYear = 2022;
+
+let birthYear = currentYear - myAge;
+
+console.log("Hello, my name is " + fullName + ". I was born in " + birthYear + ".");
